cdd7a5584d7ecbad4833f9a2f18fba069c8116ff
= <=>
2022-10-14 18:39:05 +0300

    CRUD

--- app/Console/Commands/MakeView.php
+++ app/Console/Commands/MakeView.php
@@ -55,7 +55,7 @@ class MakeView extends Path
         {
              $devTools = new DevTools();
              $devTools->setStupFileName("HeaderIndex.stub");
-             $Line =  self::OneLineInPage($NM) . "\n\n@section("."$NM".")";
+             $Line =  self::OneLineInPage($NM) . "\n\n@section("."'$NM'".")";
             $Line .= "\n\n" . $devTools->GetContentAnyFile($devTools->DevToolsName($devTools->getStupFileName()));
             return $Line;
         }
@@ -74,7 +74,7 @@ class MakeView extends Path
         {
             $Line = $this->HeaderPage($NM) . "\n" . $this->TabelPageIndex($NM);
             $NM[0] = Str::lower($NM[0]);
-            $Line .= str_replace("{{ModelSmall}}",$NM,$Line,$i);
+            $Line = str_replace("{{ModelSmall}}",$NM,$Line,$i);
             return $Line;
         }
 
@@ -124,131 +124,4 @@ class MakeView extends Path
             fwrite($fs ,$this->SetTD($NM));
             fclose($fs);
         }
-
-    //     public static function MakeFileOne($FileNameInFolder)
-    //     {
-    //         return $FileNameInFolder . ".blade.php\n";
-    //     }
-
-
-    //     /** 
-    //     *   This code for get model folder name
-    //     * @param $NM  Is model name
-    //     * F Folder
-    //     * M Model
-    //     * FMPath folder model path
-    //     */
-    //     public static function FMPath($NM)
-    //     {
-    //         return self::FolderPath($NM) . "\\";
-    //     }
-
-    //     public static function SetWidgth( $stub , $getColumnDataBase)
-    //     {
-            
-    //         $GetPathRouteName = DevTools::DevToolsName($stub);
-    //         $GetStubContentFile = DevTools::GetStubContent($GetPathRouteName);
-    //         $FinishRoute = "\n" . str_replace('{{name}}' , $getColumnDataBase ,$GetStubContentFile);
-    //         return $FinishRoute;
-    //     }
-
-
-    //     public static function GetPageName($folderNamesss,$FileNameInFolder ,$counter)
-    //     {
-    //         return  $folderNamesss.$FileNameInFolder[$counter].".blade.php";
-    //     }
-
-    //     public static function getPathPageFIle( $page , $NM )
-    //     {
-    //         return self::viewPath() . $NM . "\\$page.blade.php";
-    //     }
-
-
-    //     public static function AddSAndUpperCase($NameMoeld)
-    //     {
-    //         $AddSModels = "";
-    //         if(!($NameMoeld[ Str::length($NameMoeld) - 1] == 's'))
-    //         {
-    //             $AddSModels = $NameMoeld . 's';
-    //         }
-    //         $AddSModels[0] = Str::lower($NameMoeld[0]); 
-    //         return $AddSModels;
-    //     }
-
-    //     public static function SetPage( $FileStub  , $pageName ,$NM)
-    //     {
-    //         $AddSModels = self::AddSAndUpperCase($NM);
-    //         $columns = Schema::getColumnListing($AddSModels);
-    //             if(self::getPathPageFIle( $pageName ,$NM))
-    //             {
-    //             $fs = fopen( self::getPathPageFIle( $pageName ,$NM),'a');
-    //             for ($j=0; $j < count($columns); $j++) { 
-                    
-    //                 fwrite( $fs , self::SetWidgth($FileStub ,$columns[$j]) );
-    //             }
-
-    //                 fclose($fs);
-
-    //     }
-    // }
-    //     /**
-    //      * @param $NM  Name Model
-    //      */
-    //     public static function CreateFile($NM)
-    //     {
-
-    //         $Files = array( 'index' , 'create' , 'show' , 'edit' );
-    //         // This code for create folder model name
-    //         self::MFView($NM);
-
-    //         /**
-    //          * This page in Folder View 
-    //          */
-    //         $INDEX = 0;
-    //         $CREATE = 1;
-    //         $SHOW = 2;
-    //         $EDIT = 3;
-            
-            
-    //         // This code for create Folder by name model
-            
-            
-    //             /**
-    //              * F Folder
-    //              * M Model 
-    //              * P Path
-    //              */
-    //          $FMP =  self::FMPath($NM);
-            
-    //     for ($i = 0 ; $i < count($Files); $i++) {
-
-    //         switch ($i) {
-    //             case $CREATE:
-    //             self::SetPage( "group-form.stub" , $Files[$i]  ,  $NM);
-    //             Infos(" Page created successfully [$Files[$i]]." , 1);
-    //                 break;
-                
-    //             default:
-    //             File::put( self::GetPageName($FMP , $Files,$i),  "nabil" );
-    //             Infos(" Page created successfully [$Files[$i]]." , 1);
-    //                 break;
-    //         }
-
-    //         if($i ==  $INDEX)
-    //         {
-
-
-    //         }else{
-
-    //             }
-    //         }
-        
-
-    //     }
-
-    //     public static function SetViewFolderAndFiles($NM)
-    //     {
-    //         self::CreateFile($NM);
-    //     }
- 
     }
--- resources/views/Nabil/index.blade.php
+++ resources/views/Nabil/index.blade.php
@@ -1,6 +1,6 @@
 @extends('CRUD')
 
-@section(Nabil)
+@section('Nabil')
 
 <div class="container card p-2 m-2 bg-light">
 @if(session()->has('Success'))
@@ -20,45 +20,12 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($Nabil as ${{ModelSmall}})
+                    @foreach($Nabil as $nabil)
                     <tr>
 			            		<td> $nabil->id</td>
 		<td> $nabil->Name</td>
 		<td> $nabil->created_at</td>
 		<td> $nabil->updated_at</td>
-
-                    </tr>
-                    @endforeach
-		</tbody>
-	    </table>
-        </div>
-@endsection@extends('CRUD')
-
-@section(Nabil)
-
-<div class="container card p-2 m-2 bg-light">
-@if(session()->has('Success'))
-<div class="alert alert-success">
-{{ session()->get('success') }}
-</div>
-@endif
-<a class="btn btn-primary m-2 w-25" href="{{ route('Nabil.create')}}">Create Team</a>
-            <table class="table table-borderless table-hover ">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>Name</th>
-                        <th>created_at</th>
-                        <th>updated_at</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($Nabil as $nabil)
-                    <tr>
-			            <td> $nabil->id</td>
-                        <td> $nabil->Name</td>
-                        <td> $nabil->created_at</td>
-                        <td> $nabil->updated_at</td>
 
                     </tr>
                     @endforeach
